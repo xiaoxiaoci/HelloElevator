0fbb2192722095058fbee166a81dc89107048945
xiaoxiaoci <1300098624@qq.com>
2019-11-21 10:44:56 +0800

    init repository

---
+++ app/src/main/java/com/example/helloelevator/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloelevator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+    }
+}
